f6362f8dd3a9a3ae6095c88a20aa8005a96cbcdc
jakubwujec <jakubwujec98@gmail.com>
2019-11-28 14:49:14 +0100

    now chart is updating not refreshing

--- frontend/src/app/my-pie-chart/my-pie-chart.component.ts
+++ frontend/src/app/my-pie-chart/my-pie-chart.component.ts
@@ -1,14 +1,14 @@
-import {Component, OnChanges, OnInit, Input, SimpleChanges} from '@angular/core';
-import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
-import { Question } from '../question/question'
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {ChartDataSets, ChartOptions, ChartType} from 'chart.js';
+import {Label} from 'ng2-charts';
+import {Question} from '../question/question';
 
 @Component({
   selector: 'app-my-pie-chart',
   templateUrl: './my-pie-chart.component.html',
   styleUrls: ['./my-pie-chart.component.css']
 })
-export class MyPieChartComponent implements OnInit,OnChanges {
+export class MyPieChartComponent implements OnInit, OnChanges {
   @Input() question: Question;
   @Input() changeTrigger: number;
   public pieChartData: ChartDataSets[] = [
@@ -23,6 +23,12 @@ export class MyPieChartComponent implements OnInit,OnChanges {
 
   public pieChartType: ChartType = 'pie';
   public pieChartPlugins = [];
+  public changeType = () => {
+    const types: ChartType[] = ['line', 'bar', 'radar', 'pie', 'polarArea', 'doughnut', 'bubble', 'scatter'];
+    const i = types.indexOf(this.pieChartType)
+    this.pieChartType = types[(i + 1) % 8];
+  }
+
 
   constructor() { }
 
@@ -30,9 +36,7 @@ export class MyPieChartComponent implements OnInit,OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    this.pieChartData = [
-      { data: Array.from(this.question.answers, answer => answer.votes), label: 'Series A' },
-    ];
+    this.pieChartData[0].data = Array.from(this.question.answers, answer => answer.votes);
     this.pieChartLabels = Array.from(this.question.answers, answer => answer.option);
   }
 
